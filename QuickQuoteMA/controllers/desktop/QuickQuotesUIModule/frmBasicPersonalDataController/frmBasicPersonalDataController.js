83604fbda138dbc259603689cd71ace3233ab93b
TebohoMotlautsi69 <teboho.motlautsi@mpilo.africa>
2026-01-05 15:30:57 +0200

    Changed how the visibilily value is assignment.

--- QuickQuoteMA/controllers/desktop/QuickQuotesUIModule/frmBasicPersonalDataController/frmBasicPersonalDataController.js
+++ QuickQuoteMA/controllers/desktop/QuickQuotesUIModule/frmBasicPersonalDataController/frmBasicPersonalDataController.js
@@ -1,8 +1,8 @@
 define({
     toggleSmokerSwitch: function () {
         var showingNo = this.view.flxSmokerNo.isVisible;
-        this.view.flxSmokerNo.isVisible = !showingNo;
-        this.view.flxSmokerYes.isVisible = showingNo;
+        this.view.flxSmokerNo.setVisibility(!showingNo);
+        this.view.flxSmokerYes.setVisibility(showingNo);
     },
 
     postShow: function () {
