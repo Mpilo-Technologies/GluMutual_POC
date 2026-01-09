d9dbfb0b8c284daa1739da342315025a6ebf23a5
TebohoMotlautsi69 <teboho.motlautsi@mpilo.africa>
2026-01-09 10:03:20 +0200

    functionality to set cover on the dashboard

--- HomePageMA/controllers/desktop/HomePageUIModule/frmDashboardController/frmDashboardController.js
+++ HomePageMA/controllers/desktop/HomePageUIModule/frmDashboardController/frmDashboardController.js
@@ -1,12 +1,69 @@
-define({ 
-
-  //Type your controller code here 
+define({
+  // Type your controller code here
 
   onTestNavigation: function () {
-    var homePageModule = kony.mvc.MDAApplication.getSharedInstance().getModuleManager().getModule({
-      "appName": "HomePageMA",
-      "moduleName": "HomePageUIModule"
-    });
+    var homePageModule = kony.mvc.MDAApplication.getSharedInstance()
+      .getModuleManager()
+      .getModule({
+        appName: "HomePageMA",
+        moduleName: "HomePageUIModule",
+      });
     homePageModule.presentationController.goToNextPage("Hello, World");
-  }
+  },
+
+  postShow: function () {
+    var covers = [];
+    var coverRangesModule = kony.mvc.MDAApplication.getSharedInstance()
+      .getModuleManager() 
+      .getModule({
+        appName: "CommonMA",
+        moduleName: "CoverRanges",
+      });
+      covers = coverRangesModule.covers();
+      this.setProducts(covers);
+  },
+
+  setProducts: function (products) {
+    products = products || [];
+    var segData = [];
+    for (var i = 0; i < products.length; i += 2) {
+      var left = products[i];
+      var right = products[i + 1];
+      var row = {
+        lblLeftTitle: left.title,
+        lblLeftDesc: left.description,
+        imgLeft: { src: left.image },
+        btnLeftSelect: { text: "Select" },
+      };
+      if (right) {
+        row.lblRightTitle = right.title;
+        row.lblRightDesc = right.description;
+        row.imgRight = { src: right.image };
+        row.btnRightSelect = { text: "Select" };
+        row.flxCardRight = { isVisible: true };
+      } else {
+        row.lblRightTitle = "";
+        row.lblRightDesc = "";
+        row.imgRight = { src: "" };
+        row.btnRightSelect = { text: "" };
+        row.flxCardRight = { isVisible: false };
+      }
+      segData.push(row);
+    }
+    this.view.segCoverList.widgetDataMap = {
+      btnLeftSelect: "btnLeftSelect",
+      btnRightSelect: "btnRightSelect",
+      flx2CardRow: "flx2CardRow",
+      flxCardLeft: "flxCardLeft",
+      flxCardRight: "flxCardRight",
+      imgLeft: "imgLeft",
+      imgRight: "imgRight",
+      lblLeftDesc: "lblLeftDesc",
+      lblLeftTitle: "lblLeftTitle",
+      lblRightDesc: "lblRightDesc",
+      lblRightTitle: "lblRightTitle",
+    };
+
+    this.view.segCoverList.setData(segData);
+  },
 });
