400700404f7529d78850dca743761fe3d7dc7e8a
TebohoMotlautsi69 <teboho.motlautsi@mpilo.africa>
2026-01-09 12:30:29 +0200

    added cover id and handle index

--- CommonMA/modules/require/CoverRanges.js
+++ CommonMA/modules/require/CoverRanges.js
@@ -38,21 +38,24 @@ define(function () {
     var _covers = function() {
         var covers = [
             {
+                id: "30100",
                 title: "Life Cover",
                 image: "lifecover.png",
                 description: "Lump-sum payout to your beneficiaries.",
             },
             {   
+                id: "20200",
                 title: "Disability Cover",
                 image: "disabilitycover.png",
                 description: "Income replacement if you become disabled.",
             },
-            {
+            {   
+                id: "30103",
                 title: "Critical Illness Cover",
                 image: "criticalillness.png",
                 description: "Lump-sum payout for critical illnesses.",
             },
-            {
+            {   id: "30300",
                 title: "Income Protection",
                 image: "incomeprotection.png",
                 description: "Monthly income replacement when unable to work.",
--- HomePageMA/controllers/desktop/HomePageUIModule/frmDashboardController/frmDashboardController.js
+++ HomePageMA/controllers/desktop/HomePageUIModule/frmDashboardController/frmDashboardController.js
@@ -14,13 +14,13 @@ define({
   postShow: function () {
     var covers = [];
     var coverRangesModule = kony.mvc.MDAApplication.getSharedInstance()
-      .getModuleManager() 
+      .getModuleManager()
       .getModule({
         appName: "CommonMA",
         moduleName: "CoverRanges",
       });
-      covers = coverRangesModule.covers();
-      this.setProducts(covers);
+    covers = coverRangesModule.covers();
+    this.setProducts(covers);
   },
 
   setProducts: function (products) {
@@ -28,20 +28,23 @@ define({
     var segData = [];
     for (var i = 0; i < products.length; i += 2) {
       var left = products[i];
-      var right = products[i + 1];
+      var right = i + 1 < products.length ? products[i + 1] : null;
       var row = {
+        lblLeftId: left.id,
         lblLeftTitle: left.title,
         lblLeftDesc: left.description,
         imgLeft: { src: left.image },
         btnLeftSelect: { text: "Select" },
       };
-      if (right) {
+      if (right != null) {
+        row.lblRightId = right.id;
         row.lblRightTitle = right.title;
         row.lblRightDesc = right.description;
         row.imgRight = { src: right.image };
         row.btnRightSelect = { text: "Select" };
         row.flxCardRight = { isVisible: true };
       } else {
+        row.lblRightId = "";
         row.lblRightTitle = "";
         row.lblRightDesc = "";
         row.imgRight = { src: "" };
@@ -62,6 +65,8 @@ define({
       lblLeftTitle: "lblLeftTitle",
       lblRightDesc: "lblRightDesc",
       lblRightTitle: "lblRightTitle",
+      lblLeftId: "lblLeftId",
+      lblRightId: "lblRightId"
     };
 
     this.view.segCoverList.setData(segData);
