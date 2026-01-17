42dfa99cc80251b763b369c4f74f8be58ccf53b9
TebohoMotlautsi69 <teboho.motlautsi@mpilo.africa>
2026-01-17 13:50:52 +0200

    update the logic to set products

--- QuickQuoteMA/controllers/desktop/QuickQuotesUIModule/frmProductCoverAmountController/frmProductCoverAmountController.js
+++ QuickQuoteMA/controllers/desktop/QuickQuotesUIModule/frmProductCoverAmountController/frmProductCoverAmountController.js
@@ -1,44 +1,41 @@
-define(['CoverRanges'], function (CoverRanges) {
-    return {
-        
-        //Type your controller code here 
-        postShow: function() {    
-            var sliderConfig = CoverRanges.sliderConfig();
-            this.setupSegment(sliderConfig);
-            this.view.btnContinue.onClick = this.btnContinueonClick.bind(this);
-        },
+define(["CoverRanges", "ApplicationManager"], function (CoverRanges, applicationManager) {
+  return {
+    //Type your controller code here
+    postShow: function () {
+      var sliderConfig = CoverRanges.sliderConfig();
+      this.setupSegment(sliderConfig);
+      this.view.btnContinue.onClick = this.btnContinueonClick.bind(this);
+    },
 
-        setupSegment: function(sliderConfig) {
-            // Define the mapping between JSON and Template Widget IDs
-            this.view.segCovers.widgetDataMap = {
-                lblTitle: "product",
-                sldCover: "sliderData",
-                txtAmount: "displayAmount"
-            };
-        
-             const segData = sliderConfig.map(item => {
-                return {
-                    product: item.product,
-                    displayAmount: item.min,
-                    sliderData: {
-                        minValue: item.min,
-                        maxValue: item.max,
-                        step: item.step,
-                    selectedValue: item.min
-                    }
-                };
-            });
-            this.view.segCovers.setData(segData);
-        },
+    setupSegment: function (sliderConfig) {
+      this.view.segCovers.widgetDataMap = {
+        lblTitle: "product",
+        sldCover: "sliderData",
+        txtAmount: "displayAmount",
+      };
 
-        btnContinueonClick: function() {
-            var currForm = kony.application.getCurrentForm();
-            var segData = currForm.segCovers.data;
-            var navManager = applicationManager.getNavigationManager();
-            navManager.navigateTo({
-                "appName": "QuickQuoteMA",
-                "friendlyName": "QuickQuotesUIModule/frmQuickQuotes"
-            }, segData);
+      const segData = sliderConfig.map((item) => ({
+        product: item.product,
+        displayAmount: item.min,
+        sliderData: {
+          minValue: item.min,
+          maxValue: item.max,
+        },
+      }));
+      this.view.segCovers.setData(segData);
+    },
+
+    btnContinueonClick: function () {
+      var currForm = kony.application.getCurrentForm();
+      var segData = currForm.segCovers.data;
+      var navManager = applicationManager.getApplicationManager().getNavigationManager();
+      navManager.setCustomInfo("coverAmounts", segData);
+      navManager.navigateTo(
+        {
+          appName: "QuickQuoteMA",
+          friendlyName: "frmQuickQuotes",
         }
-    }
- });
+      );
+    },
+  };
+});
