9b91266ccbc45910b00c1b5d3ffdd2675ead6d68
TebohoMotlautsi69 <teboho.motlautsi@mpilo.africa>
2026-01-17 14:07:07 +0200

    Removed the currency sysmbol

--- CommonMA/modules/require/CoverRanges.js
+++ CommonMA/modules/require/CoverRanges.js
@@ -5,30 +5,30 @@ define(function () {
         const sliderConfig = [
             {
                 product: "Life Cover",
-                minValue: R100000,
-                maxValue: R25000000,
-                currentValue: R100000,
+                minValue: 100000,
+                maxValue: 25000000,
+                currentValue: 100000,
                 step: 50000
             },
             {     
                 product: "Disability Cover",
-                minValue: R100000,
-                maxValue: R20000000,
-                currentValue: R100000,
+                minValue: 100000,
+                maxValue: 20000000,
+                currentValue: 100000,
                 step: 50000
             },
             {   
                 product: "Critical Illness Cover",
-                minValue: R100000,
-                maxValue: R6000000,
-                currentValue: R100000,
+                minValue: 100000,
+                maxValue: 6000000,
+                currentValue: 100000,
                 step: 10000
              },
              {
                 product: "Income Protection",
-                minValue: R2000,
-                maxValue: R150000,
-                currentValue: R2000,
+                minValue: 2000,
+                maxValue: 150000,
+                currentValue: 2000,
                 step: 500
             }
             ];
