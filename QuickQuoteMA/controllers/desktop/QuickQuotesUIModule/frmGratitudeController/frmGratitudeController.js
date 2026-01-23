1012347a6ae8d7bf054d5c5273e00d3b8feb3514
TebohoMotlautsi69 <teboho.motlautsi@mpilo.africa>
2026-01-23 13:45:55 +0200

    set navigation on the confirmation screens

--- QuickQuoteMA/controllers/desktop/QuickQuotesUIModule/frmGratitudeController/frmGratitudeController.js
+++ QuickQuoteMA/controllers/desktop/QuickQuotesUIModule/frmGratitudeController/frmGratitudeController.js
@@ -1,5 +1,25 @@
-define({ 
+define({
+  //Type your controller code here
+  postShow: function () {
+    this.view.btnSecondary.onClick = this.navAnotherPage.bind(
+      this,
+      "QuickQuoteMA",
+      "frmBasicPersonalData",
+    );
 
- //Type your controller code here 
-
- });
+    this.view.btnPrimary.onClick = () => {
+      voltmx.application.openURL("https://glumutual.co.za");
+    };
+  },
+  
+  navAnotherPage: function (appName, friendlyName) {
+    var applicationManager = require("ApplicationManager");
+    var navManager = applicationManager
+      .getApplicationManager()
+      .getNavigationManager();
+    navManager.navigateTo({
+      appName: appName,
+      friendlyName: friendlyName,
+    });
+  },
+});
--- QuickQuoteMA/controllers/desktop/QuickQuotesUIModule/frmQuoteConfirmationController/frmQuoteConfirmationController.js
+++ QuickQuoteMA/controllers/desktop/QuickQuotesUIModule/frmQuoteConfirmationController/frmQuoteConfirmationController.js
@@ -1,5 +1,27 @@
-define({ 
+define({
+  //Type your controller code here
+  postShow: function () {
+    this.view.btnSecondary.onClick = this.navAnotherPage.bind(
+      this,
+      "QuickQuoteMA",
+      "frmBasicPerosalData",
+    );
 
- //Type your controller code here 
-
- });
+    this.view.btnPrimary.onClick = this.navAnotherPage.bind(
+      this,
+      "QuickQuoteMA",
+      "frmFeedback",
+    );
+  },
+  
+  navAnotherPage: function (appName, friendlyName) {
+    var applicationManager = require("ApplicationManager");
+    var navManager = applicationManager
+      .getApplicationManager()
+      .getNavigationManager();
+    navManager.navigateTo({
+      appName: appName,
+      friendlyName: friendlyName,
+    });
+  },
+});
