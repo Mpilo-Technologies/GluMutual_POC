9b1b5a9fe014fea44c6878f769c2f16dd69772a4
TebohoMotlautsi69 <teboho.motlautsi@mpilo.africa>
2026-01-27 16:48:39 +0200

    functionlity to select rating

--- QuickQuoteMA/controllers/desktop/QuickQuotesUIModule/frmFeedbackController/frmFeedbackController.js
+++ QuickQuoteMA/controllers/desktop/QuickQuotesUIModule/frmFeedbackController/frmFeedbackController.js
@@ -1,5 +1,61 @@
-define({ 
+define({
+  //Initialize form data
+  postShow: function () {
+    this.selectedRating = null;
+    this.resetRatingButtons();
+    this.attachRatingButtonHandlers();
+  },
 
- //Type your controller code here 
+  onSelectRate: function (rating) {
+    this.selectedRating = rating;
+    this.updateRatingButtonsUI(rating);
+  },
 
- });
+  updateRatingButtonsUI: function (selectedRating) {
+    for (var i = 0; i <= 10; i++) {
+      var buttonId = "btnRating" + i;
+      var button = this.view[buttonId];
+
+      if (button) {
+        if (i <= selectedRating) {
+          button.skin = "defBtnPressed";
+        } else {
+          button.skin = "sknBtnGhost";
+        }
+      }
+    }
+  },
+
+  resetRatingButtons: function () {
+    for (var i = 0; i <= 10; i++) {
+      var buttonId = "btnRating" + i;
+      var button = this.view[buttonId];
+
+      if (button) {
+        button.skin = "sknBtnGhost";
+      }
+    }
+    this.selectedRating = null;
+  },
+
+  getSelectedRating: function () {
+    return this.selectedRating;
+  },
+
+  attachRatingButtonHandlers: function () {
+    var self = this;
+
+    for (var i = 0; i <= 10; i++) {
+      (function (rating) {
+        var buttonId = "btnRating" + rating;
+        var button = self.view[buttonId];
+
+        if (button) {
+          button.onClick = function () {
+            self.onSelectRate(rating);
+          };
+        }
+      })(i);
+    }
+  },
+});
